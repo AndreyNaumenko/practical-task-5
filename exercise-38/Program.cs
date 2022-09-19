debecc933b8d685e818ca20584eee89c720a8107
Andrey <rolland81280@gmail.com>
2022-09-20 01:56:22 +0300

    сохран3

---
+++ exercise-38/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,9);
+}
+int min = array[0];
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+    if(min > array[i])
+    {
+        min = array[i];
+    } 
+    if(max < array[i])
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine($"Максимальный элемент: {max}, Минимальный элемент: {min}");
+System.Console.WriteLine($"Разница между максимальным и минимальным элементом: {max - min}");
