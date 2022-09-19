980e306e574a4d89026a70f6da42985ae79cd8c3
Andrey <rolland81280@gmail.com>
2022-09-20 01:27:43 +0300

    сохран2

---
+++ exercise-36/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10,10);
+}
+int unevenSum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+    if(i % 2 > 0)
+    {
+        unevenSum = unevenSum + array[i];
+    }
+}
+System.Console.Write("Cумма элементов, стоящих на нечётных позициях: " + unevenSum);
